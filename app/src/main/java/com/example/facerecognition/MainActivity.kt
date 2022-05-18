81b7e24db27ad4502d272bc7983bc3ccfac91c70
Veronika <veronika.smirnyagina@gmail.com>
2022-05-18 04:09:38 +0300

    Recognize in another activity + some design

--- app/src/main/java/com/example/facerecognition/MainActivity.kt
+++ app/src/main/java/com/example/facerecognition/MainActivity.kt
@@ -1,46 +1,21 @@
 package com.example.facerecognition
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.Manifest
-import android.annotation.SuppressLint
+import com.example.facerecognition.databinding.ActivityMainBinding
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.util.Log
+import android.os.Build
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.util.concurrent.Executors
-import androidx.camera.core.*
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.video.*
-import androidx.camera.video.VideoCapture
-import androidx.core.content.PermissionChecker
-import com.example.facerecognition.databinding.ActivityMainBinding
-import java.nio.ByteBuffer
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import android.provider.MediaStore
-
-import android.content.ContentValues
-import android.os.Build
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.face.FaceContour
-import com.google.mlkit.vision.face.FaceDetection
-import com.google.mlkit.vision.face.FaceDetectorOptions
-import com.google.mlkit.vision.face.FaceLandmark
-
-typealias LumaListener = (luma: Double) -> Unit
-
+import com.example.facerecognition.MainActivity.Companion.REQUEST_CODE_PERMISSIONS
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var viewBinding: ActivityMainBinding
-
-    private var imageCapture: ImageCapture? = null
-
-    private lateinit var cameraExecutor: ExecutorService
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,103 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         // Request camera permissions
         if (allPermissionsGranted()) {
-            startCamera()
+            // Set up the listeners for start rec buttons
+            viewBinding.authorizationButton.setOnClickListener { startRecognition() }
+            viewBinding.registrationButton.setOnClickListener { startRecognition() }
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        // Set up the listeners for take photo and video capture buttons
-        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
-
-        cameraExecutor = Executors.newSingleThreadExecutor()
-    }
-
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time stamped name and MediaStore entry.
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
-            }
-        }
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
-            .build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun
-                        onImageSaved(output: ImageCapture.OutputFileResults){
-                    val msg = "Photo capture succeeded: ${output.savedUri}"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            }
-        )
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
-                }
-
-            imageCapture = ImageCapture.Builder().build()
-
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                .build()
-                .also {
-                    it.setAnalyzer(
-                        cameraExecutor,
-                        LuminosityAnalyzer()
-                    )
-                }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageAnalyzer, imageCapture)
-
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -159,7 +45,8 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                startCamera()
+                viewBinding.authorizationButton.setOnClickListener { startRecognition() }
+                viewBinding.registrationButton.setOnClickListener { startRecognition() }
             } else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
@@ -169,94 +56,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
-
-        private fun ByteBuffer.toByteArray(): ByteArray {
-            rewind()    // Rewind the buffer to zero
-            val data = ByteArray(remaining())
-            get(data)   // Copy the buffer into a byte array
-            return data // Return the byte array
-        }
-
-        @SuppressLint("UnsafeOptInUsageError")
-        override fun analyze(imageProxy: ImageProxy) {
-
-            println("Analyze log message")
-
-            val mediaImage = imageProxy.image
-            if (mediaImage != null) {
-                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
-                // Pass image to an ML Kit Vision API
-
-                val options = FaceDetectorOptions.Builder()
-                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
-                    .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-                    .build()
-
-                // Get instance of face detector
-                val detector = FaceDetection.getClient(options)
-
-                val result = detector.process(image)
-                    .addOnSuccessListener { faces ->
-                        // Task completed successfully
-                        println("Task completed successfully")
-
-                        for (face in faces) {
-                            val bounds = face.boundingBox
-                            val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
-                            val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
-
-                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
-                            // nose available):
-                            val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
-                            leftEar?.let {
-                                val leftEarPos = leftEar.position
-                                println("\n leftEarPos:$leftEarPos")
-                            }
-
-                            // If contour detection was enabled:
-                            val leftEyeContour = face.getContour(FaceContour.LEFT_EYE)?.points
-                            val upperLipBottomContour = face.getContour(FaceContour.UPPER_LIP_BOTTOM)?.points
-
-                            // If classification was enabled:
-                            if (face.smilingProbability != null) {
-                                val smileProb = face.smilingProbability
-                                println("\n smileProb:$smileProb")
-                            }
-                            if (face.rightEyeOpenProbability != null) {
-                                val rightEyeOpenProb = face.rightEyeOpenProbability
-                                println("\n rightEyeOpenProb:$rightEyeOpenProb")
-                            }
-
-                            // If face tracking was enabled:
-                            if (face.trackingId != null) {
-                                val id = face.trackingId
-                            }
-                        }
-                    }
-                    .addOnFailureListener { e ->
-                        // Task failed with an exception
-                        println("Task failed with an exception")
-                    }
-                    .addOnCompleteListener {
-                        mediaImage?.close()
-                        imageProxy.close() }
-
-            }
-
-            //listener(luma)
-
-            //listener()
-
-            imageProxy.close()
-            mediaImage?.close()
-        }
+    private fun startRecognition() {
+        val intent = Intent(this, RecognizeActivity::class.java)
+        startActivity(intent)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
 
     companion object {
         private const val TAG = "CameraXApp"
@@ -264,12 +68,12 @@ class MainActivity : AppCompatActivity() {
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
-                Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO
+                Manifest.permission.CAMERA
             ).apply {
                 if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
     }
+
 }
---
+++ app/src/main/java/com/example/facerecognition/RecognizeActivity.kt
@@ -0,0 +1,198 @@
+package com.example.facerecognition
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.*
+import androidx.core.content.ContextCompat
+import com.example.facerecognition.databinding.ActivityRecognizeBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceContour
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+import com.google.mlkit.vision.face.FaceLandmark
+import java.nio.ByteBuffer
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+class RecognizeActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityRecognizeBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private lateinit var cameraExecutor: ExecutorService
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityRecognizeBinding.inflate(layoutInflater)
+
+        setContentView(viewBinding.root)
+
+        startCamera()
+
+        // Set up the listeners for stop rec buttons
+        viewBinding.recognizeStopButton.setOnClickListener { stopRecognition() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        val thread = Thread(
+            null, doBackgroundThreadProcessing,
+            "Background"
+        )
+        thread.start()
+    }
+
+    private val doBackgroundThreadProcessing = Runnable {
+        Thread.sleep(5000)
+        stopRecognition()
+    }
+
+    private fun stopRecognition() {
+        //Stop the activity
+        finish()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(
+                        cameraExecutor,
+                        FaceAnalyzer()
+                    )
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalyzer, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e("Log Info: ", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private class FaceAnalyzer : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        @SuppressLint("UnsafeOptInUsageError")
+        override fun analyze(imageProxy: ImageProxy) {
+
+            println("Analyze log message")
+
+            val mediaImage = imageProxy.image
+            if (mediaImage != null) {
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                // Pass image to an ML Kit Vision API
+
+                val options = FaceDetectorOptions.Builder()
+                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                    .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                    .build()
+
+                // Get instance of face detector
+                val detector = FaceDetection.getClient(options)
+
+                val result = detector.process(image)
+                    .addOnSuccessListener { faces ->
+                        // Task completed successfully
+                        println("Task completed successfully")
+
+                        for (face in faces) {
+                            val bounds = face.boundingBox
+                            val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+                            val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
+
+                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                            // nose available):
+                            val leftEar = face.getLandmark(FaceLandmark.LEFT_EAR)
+                            leftEar?.let {
+                                val leftEarPos = leftEar.position
+                                println("\n leftEarPos:$leftEarPos")
+                            }
+
+                            // If contour detection was enabled:
+                            val leftEyeContour = face.getContour(FaceContour.LEFT_EYE)?.points
+                            val upperLipBottomContour = face.getContour(FaceContour.UPPER_LIP_BOTTOM)?.points
+
+                            // If classification was enabled:
+                            if (face.smilingProbability != null) {
+                                val smileProb = face.smilingProbability
+                                println("\n smileProb:$smileProb")
+                            }
+                            if (face.rightEyeOpenProbability != null) {
+                                val rightEyeOpenProb = face.rightEyeOpenProbability
+                                println("\n rightEyeOpenProb:$rightEyeOpenProb")
+                            }
+
+                            // If face tracking was enabled:
+                            if (face.trackingId != null) {
+                                val id = face.trackingId
+                            }
+
+                            if (face.allContours != null)
+                            {
+                                val allContours = face.allContours
+                                println("\n allContours:$allContours")
+                            }
+                        }
+                    }
+                    .addOnFailureListener { e ->
+                        // Task failed with an exception
+                        println("Task failed with an exception")
+                    }
+                    .addOnCompleteListener {
+                        mediaImage?.close()
+                        imageProxy.close() }
+
+            }
+
+            imageProxy.close()
+            mediaImage?.close()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+
+}
